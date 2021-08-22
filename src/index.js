6788d229726aababd53823741075397504f5fca1
Veronika <veronikakrukovich@gmail.com>
2021-08-22 16:57:14 +0300

    feat: add min max avg functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,40 @@
 
 exports.min = function min (array) {
-  return 0;
+    var min = 0;
+    if(!array || array.length === 0){
+      return min;  
+    }
+    var min = array[0];
+    for (var i = 0; i < array.length; i++) {
+      if (array[i] < min) {
+        min = array[i];
+      } 
+    }
+    return min;
 }
 
 exports.max = function max (array) {
-  return 0;
+    var max = 0;
+    if(!array || array.length === 0){
+      return max;  
+    }
+    var max = array[0];
+    for (var i = 0; i < array.length; i++) {
+      if (array[i] > max) {
+        max = array[i];
+      }
+    }
+    return max;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    var avg = 0;
+    if(!array || array.length === 0){
+      return avg;  
+    }
+    var sum = 0;
+    for (var i = 0; i < array.length; i++){
+        sum += array[i];
+    }
+    return (sum / array.length);
 }
